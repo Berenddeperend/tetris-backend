4b63e53e230180f8643647f7a86c4518774e2be4
Berend <berend.swennenhuis@trimm.nl>
2021-02-28 23:16:07 +0100

    Updated some backend fields

--- index.ts
+++ index.ts
@@ -1,78 +1,45 @@
-require('dotenv').config()
-import express from 'express';
-const cors = require("cors")
-var bodyParser = require('body-parser')
+const dotenv = require("dotenv").config();
+import express from "express";
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-
-
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 const app = express();
-const pool = new Pool()
+const pool = new Pool();
 const PORT = 8000;
-
-var whitelist = ['http://example1.com', 'http://example2.com']
-
-
 app.use(cors());
-app.options('*', cors()) // include before other routes
-
+app.options("*", cors()); // include before other routes
 app.use(bodyParser.json());
+console.log("=======================================");
 
-console.log("=======================================")
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // res.sendFile('./index.html')
-  res.send('hello world')
-})
+  res.send("hello world");
+});
 
-app.get('/scores', (req, res) => {
-  const scores = pool.query(`SELECT * FROM scores`).then(scores => {
-    res.json(scores)
+app.get("/scores", (req, res) => {
+  pool.query(`SELECT * FROM scores`).then((scores) => {
+    res.json(scores);
   });
 });
 
-app.post('/score', (req, res) => {
-  console.log(req.body)
+app.post("/score", (req, res) => {
+  console.log(req.body);
+  const { name, score, date, v, mode } = req.body;
+  const insertText =
+    "INSERT INTO scores(name, score, timestamp, v, mode) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+  pool
+    .query(insertText, [name, score, date, v, mode])
+    .then((scores) => {
+      return pool.query(`SELECT * FROM scores`);
+    })
+    .then((scores) => {
+      res.json(scores);
+    });
+});
 
-  const {name, score, date} = req.body;
+// INSERT INTO score (name, score, timestamp, v, mode) VALUES ('brnd', 1, '2016-06-22 19:10:25-07', '1.0', 'singlePlayer');
 
-  const insertText = 'INSERT INTO scores(name, score, timestamp) VALUES ($1, $2, $3)'
-  pool.query(insertText, [name, score, date]).then(scores => {
-    return pool.query(`SELECT * FROM scores`);
-  }).then(scores => {
-    res.json(scores);
-  });
-
-})
-
-// INSERT INTO score (name, score, timestamp) VALUES ('brnd', 1, '2016-06-22 19:10:25-07')
-
-
-//   // pool.query('SELECT NOW()', (err, res) => {
-//   //   console.log(err, res)
-//   //   pool.end()
-//   // })
-// });
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
-
-
-// pool.end(); 
-
-
-  // pool.query(`
-  // INSERT INTO score (name, score, timestamp) VALUES ('brnd', 1, ${date});
-  // `);
-
-
-
-
-// `
-// CREATE TABLE score (
-//   id BIGSERIAL NOT NULL PRIMARY KEY,
-//   name VARCHAR(6) NOT NULL,
-//   score int NOT NULL,
-//   timestamp TIMESTAMP NOT NULL
-// );
-// `

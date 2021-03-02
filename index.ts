ee4db7df25afacce7c492f9190eee797dc02ac92
Berend <berend.swennenhuis@trimm.nl>
2021-03-02 23:03:30 +0100

    toy around with cors

--- index.ts
+++ index.ts
@@ -7,9 +7,16 @@ const { Pool } = require("pg");
 const app = express();
 const pool = new Pool();
 const PORT = 8000;
-app.use(cors());
-app.options("*", cors()); // include before other routes
+// app.use(cors());
+// app.options("*", cors()); // include before other routes
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next()
+})
+
 console.log("=======================================");
 
 app.get("/", (req, res) => {

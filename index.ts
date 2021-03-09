9eb5554a1431fd45e93d500bc9254dc2a38a09f5
Berend <berend.swennenhuis@trimm.nl>
2021-03-09 20:45:49 +0100

    static server for certbot

--- index.ts
+++ index.ts
@@ -13,37 +13,41 @@ app.options("*", cors()); // include before other routes
 
 console.log("=======================================");
 
-app.get("/", (req, res) => {
-  // res.sendFile('./index.html')
-  res.sendFile(path.join(__dirname, "./../tetris/dist", "index.html"));
 
-  // res.send("hello world");
-});
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
-app.get("/scores", (req, res) => {
-  pool.query(`SELECT * FROM scores`).then((scores) => {
-    const output = [
-      ...scores.rows.sort((a, b) => {
-        return b.score - a.score || a.timestamp - b.timestamp;
-      }),
-    ];
 
-    res.json(output);
-  });
-});
+// app.get("/", (req, res) => {
+//   // res.sendFile('./index.html')
+//   // res.sendFile(path.join(__dirname, "./../tetris/dist", "index.html"));
 
-app.post("/score", (req, res) => {
-  console.log(req.body);
+//   res.send("hello world");
+// });
 
-  const { name, score, timestamp, v, mode } = req.body;
-  const insertText =
-    "INSERT INTO scores(name, score, timestamp, v, mode) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-  pool
-    .query(insertText, [name, score, timestamp, v, mode])
-    .then((submittedScore) => {
-      res.json(submittedScore.rows[0]);
-    });
-});
+// app.get("/scores", (req, res) => {
+//   pool.query(`SELECT * FROM scores`).then((scores) => {
+//     const output = [
+//       ...scores.rows.sort((a, b) => {
+//         return b.score - a.score || a.timestamp - b.timestamp;
+//       }),
+//     ];
+
+//     res.json(output);
+//   });
+// });
+
+// app.post("/score", (req, res) => {
+//   console.log(req.body);
+
+//   const { name, score, timestamp, v, mode } = req.body;
+//   const insertText =
+//     "INSERT INTO scores(name, score, timestamp, v, mode) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+//   pool
+//     .query(insertText, [name, score, timestamp, v, mode])
+//     .then((submittedScore) => {
+//       res.json(submittedScore.rows[0]);
+//     });
+// });
 
 // INSERT INTO score (name, score, timestamp, v, mode) VALUES ('brnd', 1, '2016-06-22 19:10:25-07', '1.0', 'singlePlayer');
 

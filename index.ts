ab7faa6582d05f13250a832fea77b69b74bbef08
Berend <berend.swennenhuis@trimm.nl>
2021-03-13 23:14:23 +0100

    Fix syntax

--- index.ts
+++ index.ts
@@ -42,11 +42,11 @@ app.listen(PORT, () => {
 });
 
 
-app.get("/", rateLimit({ windowMs: 200, max: 1}, (req, res) => {
+app.get("/", rateLimit({ windowMs: 200, max: 1}), (req, res) => {
   res.send("hello world");
 });
 
-app.get("/scores", rateLimit({ windowMs: 200, max: 1}, (req, res) => {
+app.get("/scores", rateLimit({ windowMs: 200, max: 1}), (req, res) => {
   console.log(req)
   pool.query(`SELECT * FROM scores`).then((scores) => {
     const output = [
@@ -59,7 +59,7 @@ app.get("/scores", rateLimit({ windowMs: 200, max: 1}, (req, res) => {
   });
 });
 
-app.post("/score", rateLimit({ windowMs: 5000, max: 1}, (req, res) => {
+app.post("/score", rateLimit({ windowMs: 5000, max: 1}), (req, res) => {
   console.log(req.body);
 
   const timestamp = new Date().toISOString();
